7e3ca5c01294bcd7f330ec0db069ca10a43bd71f
Alex G <gvozdik.sanya@gmail.com>
2025-01-20 09:50:16 +1100

    Cleaned up deployment scripts and readme

--- node-api/src/controllers/handlerAuthenticated.ts
+++ node-api/src/controllers/handlerAuthenticated.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-// import { Pool } from 'pg';
 import { validateToken } from '../services/jwt';
 
 export default async function handlerAuthenticated(req: Request, res: Response) {
@@ -8,12 +7,4 @@ export default async function handlerAuthenticated(req: Request, res: Response) 
     } else {
         res.send(`Token validation failed; Not authenticated`);
     }
-    // const pool = new Pool({
-    //     host: env.POSTGRES_HOST,
-    //     user: env.POSTGRES_USER,
-    //     password: env.POSTGRES_PASSWORD,
-    //     database: env.POSTGRES_DB,
-    //     port: env.POSTGRES_PORT,
-    //     idleTimeoutMillis: 30000,
-    //   });
 }
