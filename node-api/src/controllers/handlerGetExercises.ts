6af70a96d70cb2f14f0ae5a5e4bf78afd9a5ca04
Alex G <gvozdik.sanya@gmail.com>
2025-02-23 16:42:02 +1100

    Do not expose user_id even though it's null always & adding spec declaration

--- node-api/src/controllers/handlerGetExercises.ts
+++ node-api/src/controllers/handlerGetExercises.ts
@@ -5,7 +5,13 @@ import { Client } from 'pg';
 export default async function handlerGetExercises(req: Request, res: Response, client: Client): Promise<void> {
     await client.connect();
     await listDefaultExercises(client).then((exercises) => {
-        res.status(200).send(exercises);
+        res.status(200).send(exercises.map((exercise) => {
+            return {
+                id: exercise.id,
+                name: exercise.name,
+                description: exercise.description
+            };
+        }));
     }).catch((error) => {
         res.status(500).send(error);
     });
